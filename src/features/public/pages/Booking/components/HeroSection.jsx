65745f49f6f72bf7daed800852f9c16de2cf942b
minnyanhtaw <minnyanhtaw07@gmail.com>
2024-12-02 19:32:13 +0700

    change breadcrumb and change font in booking page

--- src/features/public/pages/Booking/components/HeroSection.jsx
+++ src/features/public/pages/Booking/components/HeroSection.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import Breadcrumb from "./Breadcrumb";
-import backgroundImage from "../../../../../assets/images/hero-&-banner-img/banner.png";
+// import Breadcrumb from "./Breadcrumb";
+import Breadcrumb from "../../AboutUs/components/Breadcrumb";
+
+import backgroundImage from "../../../../../assets/images/hero-&-banner-img/for-hero.jpg";
 
 const HeroSection = () => {
   return (
@@ -10,7 +12,7 @@ const HeroSection = () => {
         style={{ backgroundImage: `url(${backgroundImage})` }}
       >
         <div className="text-white z-10 ">
-          <h1 className="text-3xl pl-0.5">Booking</h1>
+          <h1 className="text-3xl pl-0.5 font-custom">Booking</h1>
           <Breadcrumb currentPageTitle={"Booking"} />
         </div>
       </div>
